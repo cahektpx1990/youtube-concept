11efed4a451bcee249692e00747f04bee83ea19b
Aleksandr Losev <cahektpx1990@gmail.com>
2021-04-09 19:01:43 +0300

    last commit

--- main.js
+++ main.js
@@ -1,21 +1,45 @@
 const swiper = new Swiper('.channel-slider', {
   // Optional parameters
   loop: true,
-  slidesPerView: 6,
-
+  slidesPerView: 1,
+  spaceBetween: 20,
+  breakpoints: {
+    1900: {
+      slidesPerView: 6
+    },
+    1600: {
+      slidesPerView:5
+    },
+    1300: {
+      slidesPerView: 4
+    },
+    1100: {
+      slidesPerView: 3
+    },
+    800: {
+      slidesPerView: 2
+    },
+  },
   // Navigation arrows
   navigation: {
     nextEl: '.channel-button-next',
     prevEl: '.channel-button-prev',
-  },
-
-  
+  },  
 });
 
 const swiperTwo = new Swiper('.channel-slider-two', {
   // Optional parameters
   loop: true,
-  slidesPerView: 3,
+  slidesPerView: 1,
+  spaceBetween: 20,
+  breakpoints: {    
+    1600: {
+      slidesPerView:3
+    },    
+    1100: {
+      slidesPerView: 2
+    },    
+  }, 
 
   // Navigation arrows
   navigation: {
@@ -27,7 +51,26 @@ const swiperTwo = new Swiper('.channel-slider-two', {
 const swiperthree = new Swiper('.channel-slider-three', {
   // Optional parameters
   loop: true,
-  slidesPerView: 6,
+  slidesPerView: 1,
+  spaceBetween: 20,
+  breakpoints: {
+    1900: {
+      slidesPerView: 6
+    },
+    1600: {
+      slidesPerView:5
+    },
+    1300: {
+      slidesPerView: 4
+    },
+    1100: {
+      slidesPerView: 3
+    },
+    800: {
+      slidesPerView: 2
+    },
+  },
+  
 
   // Navigation arrows
   navigation: {
@@ -35,4 +78,16 @@ const swiperthree = new Swiper('.channel-slider-three', {
     prevEl: '.channel-button-prev-three',
   },  
 });
+
+const searchBtn = document.querySelector ('.mobile-search');
+const mobileSearch = document.querySelector ('.input__group');
+searchBtn.addEventListener('click', () => {
+  mobileSearch.classList.toggle("is-open");
+});
+
+if (document.documentElement.scrollWidth <= 640) {
+  swiper.destroy();
+  swiperTwo.destroy();
+  swiperthree.destroy();
+};
 
